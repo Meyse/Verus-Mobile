7020184784be013921a5bd0759efddf54b7cb16b
Meyse <maxtheyse@pm.me>
2025-09-10 17:29:08 +0200

    update

--- react-native.config.js
+++ react-native.config.js
@@ -2,16 +2,8 @@ module.exports = {
   dependencies: {
     "react-native-verus-light-client": {
       platforms: {
-        android: null, // disable Android platform, other platforms will still autolink if provided
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        ios: null // disable iOS platform as well
-=======
+        android: null, // disable Android platform, other platforms will still autolink if provided 
         ios: null // disable iOS platform, other platforms will still autolink if provided
->>>>>>> Stashed changes
-=======
-        ios: null // disable iOS platform, other platforms will still autolink if provided
->>>>>>> Stashed changes
       }
     }
   },
