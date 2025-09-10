7572a7b94a757b324169f9b8fa6a9f808cb037c1
Meyse <maxtheyse@pm.me>
2025-09-10 17:28:48 +0200

    update

--- react-native.config.js
+++ react-native.config.js
@@ -4,7 +4,11 @@ module.exports = {
       platforms: {
         android: null, // disable Android platform, other platforms will still autolink if provided
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         ios: null // disable iOS platform as well
+=======
+        ios: null // disable iOS platform, other platforms will still autolink if provided
+>>>>>>> Stashed changes
 =======
         ios: null // disable iOS platform, other platforms will still autolink if provided
 >>>>>>> Stashed changes
